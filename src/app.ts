72d0295c37eae9c1669bd7f5e2f06857d95aaf07
Georgii Klimov <klimovgg@gmail.com>
2022-09-13 01:59:50 +0200

    clear app.ts before server creation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,3 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-});
-app.listen(port, () => {
-    return console.log(`server is listening on ${port}`);
-}).on("error", err => console.log(err));
