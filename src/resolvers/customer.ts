a72ce4be237189b575795c97dc0d24454846b67a
Georgii Klimov <klimovgg@gmail.com>
2022-09-13 02:52:19 +0200

    create resolver for Customer

--- src/resolvers/customer.ts
+++ src/resolvers/customer.ts
@@ -1,5 +1,5 @@
-import { Query, Resolver } from 'type-graphql';
-import { Customer } from '../schema/customer';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { CreateCustomerInput, Customer } from '../schema/customer';
 import { Service } from 'typedi';
 import { CustomerService } from '../database/services/customerService';
 
@@ -11,5 +11,17 @@ export class CustomerResolver {
   @Query((returns) => [Customer], { nullable: true })
   async getCustomers(): Promise<Customer[]> {
     return await this.customerService.getAll();
+  }
+
+  @Query((returns) => Customer, { nullable: true })
+  async getCustomer(@Arg('id') id: number): Promise<Customer | undefined> {
+    return await this.customerService.getOne(id);
+  }
+
+  @Mutation((returns) => Customer)
+  async addCustomer(
+    @Arg('CustomerInput') createCustomerInput: CreateCustomerInput,
+  ): Promise<Customer> {
+    return await this.customerService.create(createCustomerInput);
   }
 }
